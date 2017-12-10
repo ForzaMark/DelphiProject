24aeff257bfca6ea778d1b10ceacd44a5917d029
13_mark <13_markheimer@web.de>
2017-12-10 17:02:02 +0100

    Update Steuerung.pas
    Ist so nicht funktionsfähig aber so könnte es aussehen

--- Steuerung.pas
+++ Steuerung.pas
@@ -39,10 +39,10 @@ procedure TForm1.FormKeyPress(Sender: TObject; var Key: char);
 begin
 
   //key ist der Knopf der Tastatur 
-  if key = 'a' then bg.Left:= bg.Left +5;
-  if key = 'd' then bg.Left := bg.Left-5;
-  if key = 'w' then bg.top := bg.top +5;
-  if key = 's' then bg.top := bg.top -5;
+  if key = 'a' and Karte1.prooveX(bg.Left) then bg.Left:= bg.Left +5;
+  if key = 'd' and Karte1.prooveY(bg.Left) then bg.Left := bg.Left-5;
+  if key = 'w' and Karte1.prooveX(bg.Left) then bg.top := bg.top +5;
+  if key = 's' and Karte1.prooveY(bg.Left) then bg.top := bg.top -5;
 
 end;
 
