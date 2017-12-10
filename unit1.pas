52f844c410b57d01e4b7baf6677af96fc7f5571d
13_mark <13_markheimer@web.de>
2017-12-10 14:18:43 +0100

    Update unit1.pas

--- unit1.pas
+++ unit1.pas
@@ -15,7 +15,6 @@ type
   TForm1 = class(TForm)
     bg: TImage;
     charakter: TSpeedButton;
-
     procedure FormKeyPress(Sender: TObject; var Key: char);
 
   private
@@ -33,11 +32,13 @@ implementation
 
 { TForm1 }
 
+// Wenn irgendwas auf der Tastatur gedrückt wird wird diese Routine angesprochen
 
 procedure TForm1.FormKeyPress(Sender: TObject; var Key: char);
 
 begin
 
+  //key ist der Knopf der Tastatur 
   if key = 'a' then bg.Left:= bg.Left +5;
   if key = 'd' then bg.Left := bg.Left-5;
   if key = 'w' then bg.top := bg.top +5;
