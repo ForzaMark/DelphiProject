51d560e3ed918cc98276082be2356b269e4d8c7e
13_mark <13_markheimer@web.de>
2017-12-11 09:20:30 +0100

    TKarte aus Unterricht

--- tkarte.pas
+++ tkarte.pas
@@ -1,45 +1,87 @@
 unit TKarte;
 
-{$mode objfpc}{$H+}
-
 interface
 
-protected
-         FPosX : Integer;
-         FPosY : Integer;
+uses ExtCtrls, Forms, SysUtils;
 
-private
-      function getPosX(): Integer;
-      function getPosY(): Integer;
-      procedure setPosX(pos : Integer);
-      procedure setPosY(pos : Integer);
+type TKarte1 = class(TImage)
 
-public
-      procedure prooveX(pos : Integer):Boolean;
-      procedure prooveY(pos : Integer):Boolean;
+  protected
+   FPosX : Integer;
+   FPosY : Integer;
+   FNichtBetretenX : Array[1..5] of Integer;
+   FNichtBetretenY: Array[1..5] of Integer;
 
-uses
-  Classes, SysUtils;
+  public
+    constructor Create(Formular : TForm; Links,Oben : Integer);
+    function getPosX():Integer;
+    function getPosY():Integer;
+    procedure setPosX(pos:Integer);
+    procedure setPosY(pos:Integer);
+    function prooveX(pos:Integer):Boolean;
+    function prooveY(pos:Integer):Boolean;
+
+end;
 
 implementation
 
-
-// Create reinpacken !!
-
-
-
-procedure prooveX(pos:Integer):Boolean
+Constructor TKarte1.Create(Formular :TForm; Links,Oben :Integer);
 begin
-         Zahl:=1;
+    inherited Create(Formular);
+
+    Left := Links;
+    Top:= Oben;
+    Parent := Formular;
+    Autosize:= True;
+    FNichtBetretenX[1]:= 0;
+
+
+
+end;
+
+function TKarte1.getPosX():Integer;
+begin
+    Result:=FPosX;
+end;
+
+function TKarte1.getPosY():Integer;
+begin
+    Result := FPosY;
+end;
+
+procedure TKarte1.setPosX(pos : Integer);
+begin
+   FPosX := pos;
+end ;
+
+procedure TKarte1.setPosY(pos : Integer);
+begin
+  FPosY := pos
+
+end;
+
+
+function TKarte1.prooveX(pos:Integer):Boolean ;
+var Zahl : Integer;
+begin
+         {Zahl:=1;
          for i:= 1 to nichtzubetreten[i].length do
          begin
              // Da muss sowas wie pos+5/pos-5 hin !!!
              if pos = nichtzubetreten[i] then Zahl := -1;
          end;
-         
+
          if Zahl = -1 then Result:= false ;
-         else Result:= true;
+         else Result:= true;}
+
+         if FNichtBetretenX[1] = pos then Result := true
+         else Result := false;
          
+end;
+
+function TKarte1.prooveY(pos:Integer):Boolean;
+begin
+
 end;
 
 end.
