81fbf0223a7de0a65bc7cdda8dc40732527904ba
13_mark <13_markheimer@web.de>
2017-12-16 14:42:29 +0100

    Add Comments to main_u nit

--- main_unit.pas
+++ main_unit.pas
@@ -6,21 +6,21 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  Buttons, ExtCtrls, TKarte, TSpielfigur, TBot;
+  Buttons, ExtCtrls, TKarte, TSpielfigur, TBot; 
+  // die 3 Klassen gibt es im Steuerung-Branch
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Edit1: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
+    // Wie schon erwähnt es brauch nur einen Timer mit dem Namen 'Timer1' als Komponente
     Timer1: TTimer;
 
     procedure FormCreate(Sender: TObject);
     procedure FormKeyPress(Sender: TObject; var Key: char);
     procedure Timer1Timer(Sender: TObject);
+    
   private
 
   public
@@ -42,10 +42,17 @@ var Bot,secBot :TBot1;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+
+   // Erschaffen von den 3 Klassen (Karte,Spielfigur,Bot)
+   // Davon 2 Bots
    Karte := TKarte1.Create(Form1,0,0);
    Karte.paintbackground('solidback.bmp');
    Spielfigur := TSpielfigur1.Create(Form1,240,176) ;
+   
+   // setFightDistance muss hier noch ausgeführt werden um später den Kampfeintritt zum richtigen Zeitpunkt zu bekommen
    Bot := TBot1.Create(Form1,240,100) ;
+   // hier heißt das soviel wie "Es wird erst eingetreten wenn die Figur 0 in x und 21 in y-Richtung entfernt ist"
+   // wird später noch klarer
    Bot.setFightDistanceX(0);
    Bot.setFightDistanceY(21);
 
@@ -62,26 +69,23 @@ end;
 procedure TForm1.FormKeyPress(Sender: TObject; var Key: char);
 begin
 
-
-
-
-
-
-
-   if (key = 'a') and Karte.prooveX(Karte.getPosX +5) then
+     if (key = 'a') and Karte.prooveX(Karte.getPosX +5) then
    begin
-       Bot.setPosX(5);
+       // passiert in allen if-Abfragen das selbe : Karte bewegt sich mit den Bots + Spielfigur wird korrekt gesetzt
        Karte.setPosX(5);
        Spielfigur.paintFigur(1);
+       Bot.setPosX(5);
+       
+       // Bei diesen setDistance Sachen wird der Abstand zwischen Spielfigur und Bot berechnet
        Bot.setDistanceX(Spielfigur.getLeft);
        Bot.setDistanceY(Spielfigur.getTop());
 
        secBot.setPosX(5);
        secBot.setDistanceX(Spielfigur.getLeft);
        secBot.setDistanceY(Spielfigur.getTop());
-
-
    end;
+   
+   
     if (key = 'd') and Karte.prooveX(Karte.getPosX -5) then
    begin
        Karte.setPosX(-5);
@@ -119,22 +123,22 @@ begin
        secBot.setDistanceX(Spielfigur.getLeft);
        secBot.setDistanceY(Spielfigur.getTop());
    end;
-
-
-
+// Das war im wesentlichen der Bewegungsapparat des Spiels
 end;
 
+
+// Hier kommt noch der Eintritt in den Kampf
 procedure TForm1.Timer1Timer(Sender: TObject);
 begin
 
-   Edit1.Text:= IntToStr(secBot.getDistanceX());
-   Edit2.Text:= IntToStr(secBot.getDistanceY());
+// Wenn die momentane Distance der Objekte (Spielfigur,Bot) kleiner als die oben definierte FightDistance ist wird der Timer angehalten
+// und es wird in der ShowMessage 'Lass uns kämpfen ausgegeben'
 
-
-if (abs(Bot.getDistanceY()) <= abs(Bot.getFightDistanceY())) and (abs(Bot.getDistanceX()) <= abs(Bot.getFightDistanceX())) then
+    if (abs(Bot.getDistanceY()) <= abs(Bot.getFightDistanceY())) and (abs(Bot.getDistanceX()) <= abs(Bot.getFightDistanceX())) then
    begin
        Timer1.Enabled:= false;
        ShowMessage(Bot.startConversation());
+       //Hier könnte Tobias einsteigen und die Überleitung in die "Kampfarena" bringen
    end;
 
 
