ba2e1b191e20fdab8852fc15e3e68009185e75d4
13_mark <13_markheimer@web.de>
2018-02-12 09:01:09 +0100

    Update Unit1

--- Unit1.pas
+++ Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, TBot,TSpielfigur,TKarte, ExtCtrls, StdCtrls,uTFightSystem,uTPokemon,pngimage;
+  Dialogs, TBot,TSpielfigur,TKarte, ExtCtrls, StdCtrls,uTFightInterface,uTPokemon,pngimage;
   //pngimage kommt von Vorlagen/Informatik/ga-if/12/BeispielPNG ... Zu genaueren Quellen
   //Herr Markert fragen
 
@@ -24,14 +24,14 @@ var
 
 implementation
 
-uses Unit2;
+
 
 {$R *.dfm}
 
 var Karte : TKarte1;
 var Spielfigur : TSpielfigur1;
 var Bot : TBot1;
-var Kampf : TFightSystem;
+var Kampf : TFightInterface;
 var Pokemon1,Pokemon2 : TPokemon;
 
 
@@ -51,6 +51,8 @@ begin
 end;
 
 procedure TForm1.FormKeyPress(Sender: TObject; var Key: Char);
+
+var AttackButton : Array[1..4] of TButton;
 begin
 
 
@@ -96,7 +98,7 @@ begin
     end;
 
 
-    if (Key = 'x') and (Karte.getPosX = -365) and (Karte.getPosY = -50) then ShowMessage('Schild');
+    if (Key = 'x') and (Karte.getPosX = -365) and (Karte.getPosY = -50) then ShowMessage('Sie befinden sich in der Schulstraﬂe\n Gehen sie auf das blaue Feld um auf den Schulhof zu gelangen');
 
 
     if (Key = 'x') and (abs(Bot.getDistanceY()) <= abs(Bot.getFightDistanceY())) and (abs(Bot.getDistanceX()) <= abs(Bot.getFightDistanceX())) then
@@ -105,15 +107,51 @@ begin
        
        ShowMessage(Bot.startConversation());
 
-       Kampf := TFightSystem.Create(Form1,0,0);
+       Kampf := TFightInterface.Create(Form1,0,0);
        Pokemon1 := TPokemon.create(Form1,1,3);
        Pokemon2 := TPokemon.create(Form1,2,1);
 
        Pokemon1.drawPokemon(Form1,130,10);
        Pokemon2.drawPokemon(Form1,20,100);
 
-       Form1.ClientHeight:= 230;
-       Form1.ClientWidth:= 250;
+       Form1.ClientHeight:= 720;
+       Form1.ClientWidth:= 720;
+
+       begin
+
+
+  AttackButton[1]:=TButton.Create(Self);
+  AttackButton[1].Parent:=Self;
+  AttackButton[1].Caption:='Attacke 1';
+  AttackButton[1].Left:=40;
+  AttackButton[1].Top:=550;
+  AttackButton[1].Width:=250;
+  AttackButton[1].Height:=40;
+
+  AttackButton[2]:=TButton.Create(Self);
+  AttackButton[2].Parent:=Self;
+  AttackButton[2].Caption:='Attacke 2';
+  AttackButton[2].Left:=420;
+  AttackButton[2].Top:=550;
+  AttackButton[2].Width:=250;
+  AttackButton[2].Height:=40;
+
+  AttackButton[3]:=TButton.Create(Self);
+  AttackButton[3].Parent:=Self;
+  AttackButton[3].Caption:='Attacke 3';
+  AttackButton[3].Left:=40;
+  AttackButton[3].Top:=645;
+  AttackButton[3].Width:=250;
+  AttackButton[3].Height:=40;
+
+  AttackButton[4]:=TButton.Create(Self);
+  AttackButton[4].Parent:=Self;
+  AttackButton[4].Caption:='Attacke 4';
+  AttackButton[4].Left:=420;
+  AttackButton[4].Top:=645;
+  AttackButton[4].Width:=250;
+  AttackButton[4].Height:=40;
+end;
 
       end;
 
